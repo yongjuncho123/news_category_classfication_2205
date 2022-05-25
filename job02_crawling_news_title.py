759d62f5d81f452cdf4b2a01bcd353f52be04347
scolpig <scolpig@gmail.com>
2022-05-25 17:20:28 +0900

    crawling 03

--- job02_crawling_news_title.py
+++ job02_crawling_news_title.py
@@ -4,6 +4,8 @@ import pandas as pd
 import re
 import time
 
+
+category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
 pages = [110, 110, 110, 78, 110, 66]
 
 url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=100#&date=%2000:00:00&page=1'
@@ -14,20 +16,45 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('disable-gpu')
 driver = webdriver.Chrome('./chromedriver', options=options)
-
+df_titles = pd.DataFrame()
 for i in range(0, 6):
     titles = []
     for j in range(1,pages[i]+1):
         url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}#&date=%2000:00:00&page={}'.format(i, j)
         driver.get(url)
-        time.sleep(2)
+        time.sleep(0.2)
+
         for k in range(1, 5):
             for l in range(1, 6):
                 x_path = '//*[@id="section_body"]/ul[{}]/li[{}]/dl/dt[2]/a'.format(k, l)
-                title = driver.find_element_by_xpath(x_path).text
-                print(title)
+                try:
+                    title = driver.find_element_by_xpath(x_path).text
+                    title = re.compile('[^가-힣 ]').sub('', title)
+                    titles.append(title)
+                except NoSuchElementException as e:
+                    print(e)
+                    print(category[i], j, 'page', k * l)
+                except StaleElementReferenceException as e:
+                    print(e)
+                    print(category[i], j, 'page', k * l)
+                except:
+                    print('error')
+        if j % 30 == 0:
+            df_section_titles = pd.DataFrame(titles, columns=['titles'])
+            df_section_titles['category'] = category[i]
+            df_titles = pd.concat([df_titles, df_section_titles], ignore_index=True)
+            df_titles.to_csv('./crawling_data_{}_{}_{}.csv'.format(category[i], j-29, j), index=False)
+            titles = []
+    df_section_titles = pd.DataFrame(titles, columns=['titles'])
+    df_section_titles['category'] = category[i]
+    df_titles = pd.concat([df_titles, df_section_titles], ignore_index=True)
+    df_titles.to_csv('./crawling_data_{}_last.csv'.format(category[i]), index=False)
+    titles = []
+driver.close()
 
 
+#//*[@id="section_body"]/ul[3]/li[1]/dl/dt/a
+#//*[@id="section_body"]/ul[2]/li[5]/dl/dt[2]/a
 
 
 
